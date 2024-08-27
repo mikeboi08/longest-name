3a826903a181d0a45ed9be7ebcaea7e5a5be7467
Micah Howard <micahhoward99@gmail.com>
2024-08-26 20:39:19 -0400

    different cases working

--- script.js
+++ script.js
@@ -11,13 +11,29 @@ for (let step = 0; step < 3; step++) {
 console.log(names);
 
 let longest_name = "";
+let longest_names=[]
 
-names.forEach((name, index) => {
-    console.log("The length of" + name + " is: " + name.length )
-    if (name.length > longest_name.length) {
-        longest_name = name;
+names.forEach((name) => {
+    console.log("The length of " + name + " is: " + name.length )
+   
+ if (name.length > longest_name.length) {
+        longest_name = name 
+        longest_names = [name];
 
     }
-});
+    else if (name.length === longest_name.length){
 
-console.log("The longest name is:", longest_name);
+        longest_names.push(name)
+        // add each to its own list and print both ?
+        
+    }
+});
+if (longest_names.length >1 && longest_names.length<3){
+    console.log("Both "+ longest_names[0] + " and "+ longest_names[1] + " are the longest names ")
+}else if (longest_names.length === 3){
+    console.log("All of the names are the same length")
+}else{
+    console.log("The longest name is:", longest_name);
+}
+
+
